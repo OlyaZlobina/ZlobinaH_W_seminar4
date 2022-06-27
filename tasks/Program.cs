c359c9128631614e65b7789ddc6a173e5de3eacf
Olya <Zlobinaolya87@gmail.com>
2022-06-27 10:09:40 +0300

    first task done

--- tasks/Program.cs
+++ tasks/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+void degreeOfNumber(int first, int second)
+{   
+    int result = 1;
+    for (int i = 0; i < second; i++)
+    {
+        result = result * first;
+    }
+    Console.Write(first + "," + second + "->" + " " + result + " ");
+}
+Console.WriteLine("Input number A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+degreeOfNumber(A, B);
