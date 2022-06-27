a9dee7a4a10dd232ac90c2f53216f0d2437d6822
Olya <Zlobinaolya87@gmail.com>
2022-06-27 10:26:35 +0300

    second task done

--- tasks/Program.cs
+++ tasks/Program.cs
@@ -1,16 +1,37 @@
 ﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
-void degreeOfNumber(int first, int second)
-{   
-    int result = 1;
-    for (int i = 0; i < second; i++)
+// void degreeOfNumber(int first, int second)
+// {   
+//     int result = 1;
+//     for (int i = 0; i < second; i++)
+//     {
+//         result = result * first;
+//     }
+//     Console.Write(first + "," + second + "->" + " " + result + " ");
+// }
+// Console.WriteLine("Input number A: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input number B: ");
+// int B = Convert.ToInt32(Console.ReadLine());
+// degreeOfNumber(A, B);
+
+
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int summOfdigites(int number)
+{
+    int num1 = number / 1000 %10;
+    int num2 = number / 100 %10;
+    int num3 = number / 10 %10;
+    int num4 = number %10;
+    int summ = 0;
+    if (number > 0)
     {
-        result = result * first;
+        summ = num4 + num3 + num2 + num1;
+        Console.WriteLine(number + " -> " + summ + " ");
     }
-    Console.Write(first + "," + second + "->" + " " + result + " ");
+    return summ;
 }
-Console.WriteLine("Input number A: ");
-int A = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input number B: ");
-int B = Convert.ToInt32(Console.ReadLine());
-degreeOfNumber(A, B);
+Console.WriteLine("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+summOfdigites(num);
