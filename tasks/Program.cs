82c812d739ced94ae1e83465faf49bed22ea81ba
Olya <Zlobinaolya87@gmail.com>
2022-06-27 10:44:10 +0300

    all tasks done

--- tasks/Program.cs
+++ tasks/Program.cs
@@ -1,19 +1,19 @@
 ﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
-// void degreeOfNumber(int first, int second)
-// {   
-//     int result = 1;
-//     for (int i = 0; i < second; i++)
-//     {
-//         result = result * first;
-//     }
-//     Console.Write(first + "," + second + "->" + " " + result + " ");
-// }
-// Console.WriteLine("Input number A: ");
-// int A = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Input number B: ");
-// int B = Convert.ToInt32(Console.ReadLine());
-// degreeOfNumber(A, B);
+void degreeOfNumber(int first, int second)
+{   
+    int result = 1;
+    for (int i = 0; i < second; i++)
+    {
+        result = result * first;
+    }
+    Console.Write(first + "," + second + "->" + " " + result + " ");
+}
+Console.WriteLine("Input number A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+degreeOfNumber(A, B);
 
 
 // Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
@@ -34,4 +34,30 @@ int summOfdigites(int number)
 }
 Console.WriteLine("Input number: ");
 int num = Convert.ToInt32(Console.ReadLine());
-summOfdigites(num);
+ summOfdigites(num);
+
+
+// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] eightElementsArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(1, 10); 
+    }
+    return array;
+}
+
+void ShowEightElementsArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+int[] myarray = eightElementsArray(8);
+ShowEightElementsArray(myarray);
+Console.Write(" ->  [");
+ShowEightElementsArray(myarray);
+Console.WriteLine("] ");
